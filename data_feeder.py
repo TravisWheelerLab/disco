6063c7d704fc73852672c267195189a1eb83cdd1
kaylairish <kayla.a.irish@gmail.com>
2021-06-15 17:18:55 -0600

    Complete initialization for DataSet class

--- data_feeder.py
+++ data_feeder.py
@@ -2,62 +2,43 @@ from glob import glob
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import defaultdict
 
 
 class SpectrogramDataset:
     def __init__(self,
-                 directory_name):  # String - "train_data" or "test_data"
-
+                 directory_name):
         self.directory_name = directory_name
-        # spectrograms_list[0] is the label, [1] is the spect.
-        self.spectrograms_list, self.max_spec_length = self.load_in_all_files(self.directory_name)
+        # spectrograms_list[i][0] is the label, [i][1] is the spect.
+        self.spectrograms_list, self.max_spec_length, self.unique_labels = self.load_in_all_files(self.directory_name)
 
-    def load_in_all_files(self, directory_name):
+    def load_in_all_files(self, directory_name, filterlabel="Y"):
         max_length = 999999
         spectrograms_list = []
         root = './' + directory_name + "/spect"
         files = glob(os.path.join(root, "*"))
-        i = 0
-        j = 0
-        class_counter = {"A": 0, "B": 0, "C": 0, "X":0, "Y":0}
-        running_length_sum = 0
+        class_counter = defaultdict(int)
         for filepath in files:
             head, tail = os.path.split(filepath)
             label = tail.split(".")[0]
             spect = np.load(filepath)
-            minval = 50
-            running_length_sum += spect.shape[1]
-            if spect.shape[1] < max_length:
-                print(spect.shape[1], head + "/" + tail)
-            if spect.shape[1] < minval:
-                i += 1
-                if label == "A" or "B" or "C" or "X" or "Y":
-                    class_counter[label] += 1
-                else:
-                    print(label)
-            max_length = min(max_length, spect.shape[1])
-            spectrograms_list.append([label, spect])
-            j += 1
-        print(i, "are smaller than", minval, "columns out of", j)
-        print(class_counter)
-        print ("average size of all:", running_length_sum/len(spectrograms_list))
-        return spectrograms_list, max_length
+            if label != filterlabel:
+                class_counter[label] += 1
+                max_length = min(max_length, spect.shape[1])
+                spectrograms_list.append([label, spect])
+        return spectrograms_list, max_length, class_counter
 
-    def __getitem__(self, index, load_all=True):
-        pass
-        # s = get_spectrogram_from_list()
-        # sub_s = get_random_contiguous_sample_from_spect(s)
-        # return sub_s, label_of_s
+    def __getitem__(self, idx):
+        label = self.spectrograms_list[idx][0]
+        spect = self.spectrograms_list[idx][1]
+        return label, spect
 
     def __len__(self):
-        return len(self.sound_labels)
+        return len(self.spectrograms_list)
 
     def generate_bar_chart(self):
-        labels_dict = dict.fromkeys(self.get_unique_labels(), 0)
-        for spect in self.spectrograms_list:
-            labels_dict[spect[0]] += 1
-        labels = list(labels_dict.keys())
-        counts = list(labels_dict.values())
+        labels = list(self.unique_labels.keys())
+        counts = list(self.unique_labels.values())
         plt.bar(labels, counts)
         plt.show()
 
@@ -71,5 +52,10 @@ class SpectrogramDataset:
 
 
 if __name__ == '__main__':
-    my_tester = SpectrogramDataset("train_data")
-    my_tester.generate_bar_chart()
+    train_data = SpectrogramDataset("train_data")
+    train_data.generate_bar_chart()
+    print(len(train_data))
+
+    test_data = SpectrogramDataset("test_data")
+    test_data.generate_bar_chart()
+    print(len(test_data))
