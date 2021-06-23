fe2fd586a311e236cf3423608ee14b199eefeb91
kaylairish <kayla.a.irish@gmail.com>
2021-06-22 20:59:32 -0600

    Add functionality to save spectrogram images to a directory

---
+++ open_and_look.py
@@ -0,0 +1,25 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from glob import glob
+
+# this function offloads spectrograms from a given root directory as images into a given folder
+# for viewing remotely plotted images.
+
+if __name__ == '__main__':
+    target = 'A'
+    root = './test_data/spect'
+    files = glob(os.path.join(root, "*"))
+    files = [f for f in files if target in f]
+
+    i = 0
+    for f in files:
+        if i > 5:
+            break
+        arr = np.load(f)
+        print(arr.shape)
+        plt.imshow(arr[:, 35:])
+        plt.show()
+        i += 1
+
+        plt.savefig('image_offload/'+target+str(i)+'.png')
