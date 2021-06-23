03e601e2ec58666c413e66a5673979cfd84bf74f
kaylairish <kayla.a.irish@gmail.com>
2021-06-22 19:57:48 -0600

    Modify saving to handle one file at a time rather than all at once

--- data_loader.py
+++ data_loader.py
@@ -33,69 +33,7 @@ class Sound:
             self.spectrogram_clip)
 
 
-def split_train_test(beetle_files_dict, train_pct=80, file_wise=False, k_means_cutoff=False, bci=45, eci=65):
-    # places beetle files' individual labeled sounds into an array holding marked train and test labels
-    # designated by each sound object's "dataset" attribute so they can eventually be fed into directories.
-    if file_wise:  # the case of reserving an extra file as the test data set
-        sound_objects = split_filewise(beetle_files_dict, train_pct, cutoff=k_means_cutoff, bci=bci, eci=eci)
-    else:  # the case of shuffling all examples within files, random chirps will go into each test set.
-        sound_objects = split_examplewise(beetle_files_dict, train_pct, cutoff=k_means_cutoff, bci=bci, eci=eci)
-    return sound_objects
-
-
-def split_filewise(beetle_files_dict, train_pct, cutoff, bci, eci):
-    # used if it is more desirable to reserve an entire beetle courtship as the test data rather than examples from
-    # multiple courtships that the neural network would have seen before.
-    if len(beetle_files_dict) == 0:
-        raise ValueError("File splitter expected at least 1 file but was given 0 files.")
-    if len(beetle_files_dict) == 1:
-        warnings.warn("File splitter expected more than 1 file but was given 1. Test and training data will not split.")
-
-    sound_objects = []
-    test_files_num = max(1, math.floor((1 - train_pct / 100) * len(beetle_files_dict)))
-
-    filename_keys = list(beetle_files_dict.keys())
-    random.shuffle(filename_keys)
-
-    for i in range(len(filename_keys)):
-        file = filename_keys[i]
-        beetlefile_object = beetle_files_dict[filename_keys[i]]
-        if cutoff and file != '1_M14F15_8_7':
-            cutoff_kmeans_spectrograms(beetlefile_object, bci, eci)
-        for sound_type, spectrogram_list in beetlefile_object.label_to_spectrogram.items():
-            for example in spectrogram_list:
-                if isinstance(example, torch.Tensor):
-                    example = example.numpy()
-                if i < test_files_num:
-                    sound_objects.append(Sound(sound_type, file, example, t.time(), dataset="test_data"))
-                else:
-                    sound_objects.append(Sound(sound_type, file, example, t.time()))
-    return sound_objects
-
-
-def split_examplewise(beetle_files_dict, train_pct, cutoff, bci, eci):
-    # used if it is more desirable to shuffle all chirps across beetles and split them into the train and test sets.
-    if len(beetle_files_dict) == 0:
-        raise ValueError("File splitter expected at least 1 file but was given 0 files.")
-
-    sound_objects = []
-
-    for file, beetlefile_object in beetle_files_dict.items():
-        if cutoff and file != '1_M14F15_8_7':
-            cutoff_kmeans_spectrograms(beetlefile_object, bci, eci)
-        for sound_type, spectrogram_list in beetlefile_object.label_to_spectrogram.items():
-            for example in spectrogram_list:
-                sound_objects.append(Sound(sound_type, file, example, t.time()))
-        print(file, "done appending.")
-    random.shuffle(sound_objects)
-    test_data_cutoff = round(len(sound_objects) * (100 - train_pct) / 100)
-    for example_index in range(test_data_cutoff):
-        sound_objects[example_index].dataset = "test_data"
-
-    return sound_objects
-
-
-def cutoff_kmeans_spectrograms(bf_obj, bci, eci):
+def cutoff_kmeans_spectrograms(bf_obj, bci=45, eci=65):
     # locates the first spectrogram column of high intensity in a given spectrogram using 2-means clustering
     # and clips the spectrogram at that spot. This removes the milliseconds of human error background at the beginning
     # of a chirp that is not actually the chirp.
@@ -117,10 +55,71 @@ def cutoff_kmeans_spectrograms(bf_obj, bci, eci):
                 bf_obj.label_to_spectrogram[sound_type][index] = spect[:, first_hi:]
 
 
-def export_all_sounds(sound_objects_list):
+def save_all_sounds(sound_objects_list):
     # simple function that calls save for each sound file in a list of sound objects.
     for sound in sound_objects_list:
         sound.save()
+
+
+def offload(beetle_object, file_wise, classes_excluded, train_pct, data_set):
+    # saves each sound from each file into a sound object array depending on filewise/examplewise organization,
+    # then saves entire array at the end.
+    sound_objects = []
+    if file_wise:
+        for sound_type, spectrogram_list in beetle_object.label_to_spectrogram.items():
+            for example in spectrogram_list:
+                if isinstance(example, torch.Tensor):
+                    example = example.numpy()
+                if sound_type not in classes_excluded:
+                    sound_objects.append(Sound(sound_type, beetle_object.name, example, t.time(), dataset=data_set))
+        print(beetle_object.name, "done appending.")
+    else:
+        for sound_type, spectrogram_list in beetle_object.label_to_spectrogram.items():
+            for example in spectrogram_list:
+                if sound_type not in classes_excluded:
+                    sound_objects.append(Sound(sound_type, beetle_object.name, example, t.time()))
+        print(beetle_object.name, "done appending.")
+        random.shuffle(sound_objects)
+        test_data_cutoff = round(len(sound_objects) * (100 - train_pct) / 100)
+        for example_index in range(test_data_cutoff):
+            sound_objects[example_index].dataset = "test_data"
+    save_all_sounds(sound_objects)
+
+
+def determine_filewise_locations(filenames, train_pct):
+    # if filewise, creates a boolean array that determines which file(s) will be test/train.
+    test_set_marker = []
+    n_test_files = max(1, math.floor((1 - train_pct / 100) * len(csvs_and_wav.keys())))
+    for i in range(len(filenames)):
+        if i < n_test_files:
+            test_set_marker.append(True)
+        else:
+            test_set_marker.append(False)
+    random.shuffle(test_set_marker)
+    return test_set_marker
+
+
+def offload_data(csvs_and_wav_files, file_wise=False, classes_excluded=[], cutoff=True, train_pct=80):
+    # this function ensures that the function saving these files (offload) has the correct information about where each
+    # sound from each file goes.
+    test_set_marker = determine_filewise_locations(csvs_and_wav_files.keys(), train_pct)
+
+    idx_test_arr = 0
+    for filename, (wav, csv) in csvs_and_wav.items():
+        spectrogram, label_to_spectrogram = sa.process_wav_file(wav, csv)
+        file = sa.BeetleFile(filename, csv, wav, spectrogram, label_to_spectrogram)
+        if cutoff and file != '1_M14F15_8_7':
+            cutoff_kmeans_spectrograms(file)
+        if file_wise:
+            if test_set_marker[idx_test_arr]:
+                data_set = "test_data"
+            else:
+                data_set = "train_data"
+        else:
+            data_set = None
+        offload(file, file_wise, classes_excluded=classes_excluded, train_pct=train_pct, data_set=data_set)
+        print(filename, "done processing.")
+        idx_test_arr += 1
 
 
 if __name__ == '__main__':
@@ -130,15 +129,7 @@ if __name__ == '__main__':
     csvs_and_wav = sa.load_csv_and_wav_files_from_directory(data_dir)
     print("csvs and wav files loaded in.")
 
-    beetle_files = {}
-
-    for filename, (wav, csv) in csvs_and_wav.items():
-        spectrogram, label_to_spectrogram = sa.process_wav_file(wav, csv)
-        beetle_files[filename] = sa.BeetleFile(filename, csv, wav, spectrogram, label_to_spectrogram)
-        print(filename, "done processing.")
-
-    sounds = split_train_test(beetle_files, file_wise=False, k_means_cutoff=True)
-    export_all_sounds(sounds)
+    offload_data(csvs_and_wav, file_wise=False, classes_excluded=['C', 'Y'])
 
     end_time = t.time()
     print('Elapsed time is %f seconds.' % (end_time - begin_time))
