6bcda65bed969ccdfa336419c40ae09a95ae6c8e
kaylairish <kayla.a.irish@gmail.com>
2021-06-23 14:05:22 -0600

    Edit getitem to return properly formatted spect tensors

--- data_feeder.py
+++ data_feeder.py
@@ -4,13 +4,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 from collections import defaultdict, OrderedDict
 import random
+import torch
 import pdb
 
-class SpectrogramDataset:
+LABEL_TO_INDEX = {'A': 0, 'B': 1, 'X': 2}
+
+
+class SpectrogramDataset(torch.utils.data.Dataset):
+
     def __init__(self,
                  directory_name,
                  max_spec_length=40,
-                 filtered_sounds=['C','Y']):
+                 filtered_sounds=['C', 'Y']):
         self.directory_name = directory_name
         self.max_spec_length = max_spec_length
         self.filtered_sounds = filtered_sounds
@@ -22,6 +27,7 @@ class SpectrogramDataset:
         # sound class, which is mainly meant for creating the bar chart. also ensures filtering of unwanted labels.
         spectrograms_list = []
         root = './' + directory_name + "/spect"
+        # root = os.path.join(directory_name, 'spect')
         files = glob(os.path.join(root, "*"))
         class_counter = defaultdict(int)
         for filepath in files:
@@ -36,13 +42,14 @@ class SpectrogramDataset:
 
     def __getitem__(self, idx):
         # returns a tuple with [0] the class label and [1] the spectrogram slice
-        # which is the size of self.max_spec_length.
+        # which is based on self.max_spec_length.
         label = self.spectrograms_list[idx][0]
         spect = self.spectrograms_list[idx][1]
         num_col = spect.shape[1]
-        random_idx = round(random.uniform(0, num_col-self.max_spec_length))
-        breakpoint()
-        return label, spect[:, random_idx:random_idx + self.max_spec_length]
+        random_index = round(random.uniform(0, num_col - self.max_spec_length))
+        # breakpoint()
+        return torch.tensor(spect[:, random_index:random_index + self.max_spec_length]).unsqueeze(0), torch.tensor(
+            LABEL_TO_INDEX[label])
 
     def __len__(self):
         return len(self.spectrograms_list)
@@ -52,7 +59,7 @@ class SpectrogramDataset:
         labels = list(self.unique_labels.keys())
         counts = list(self.unique_labels.values())
         plt.bar(labels, counts)
-        plt.savefig('image_offload/'+"bar_chart_"+self.directory_name+'.png')
+        plt.savefig('image_offload/' + "bar_chart_" + self.directory_name + '.png')
 
     def get_unique_labels(self):
         return self.unique_labels.keys()
