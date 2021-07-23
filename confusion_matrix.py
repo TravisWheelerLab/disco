080b647fcffd7d33a42f65eddd2137e7150215f8
kaylairish <kayla.a.irish@gmail.com>
2021-07-23 15:23:34 -0700

    Change function to save all three matrices

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -3,43 +3,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 import itertools
 
+
 class ConfusionMatrix:
 
     def __init__(self, num_classes=3):
         self.matrix = np.zeros((num_classes, num_classes), dtype=np.int64)
 
-    def increment_conf_matrix(self, target, pred):
+    def increment(self, target, pred):
+        # takes in target, the true labels, and pred, the predicted labels, for each time point in a given
+        # spectrogram, and maps those labels to their correct places in the confusion matrix.
         stacked = torch.stack((target, pred), dim=1).squeeze().numpy()
-        for i in range(stacked.shape[0]):
+        for i in range(stacked.shape[1]):
             true_label = stacked[0][i]
             predicted_label = stacked[1][i]
             self.matrix[true_label, predicted_label] += 1
 
-    def plot(self, classes, normalize=False, title='Confusion matrix', cmap='Blues'):
-        if normalize:
-            print(self.matrix)
-            self.matrix = self.matrix.astype('float') / self.matrix.sum(axis=1)[:, np.newaxis]
-            np.round(self.matrix, 3)
-            print('Normalized confusion matrix.')
-        else:
-            print('Confusion matrix, without normalization.')
+    def plot(self, classes):
+        # this function generates and saves images of the three confusion matrices: counts and normalized across
+        # rows/cols.
 
-        print(self.matrix)
-        plt.imshow(self.matrix, interpolation='nearest', cmap=cmap)
-        plt.title(title)
-        plt.colorbar()
-        tick_marks = np.arange(len(classes))
-        plt.xticks(tick_marks, classes, rotation=45)
-        plt.yticks(tick_marks, classes)
+        # Creates three plots: totals, row-normalized (recall), and column-normalized (precision)
+        print('UNNORMALIZED:\n', self.matrix)
+        row_normalized_mat = np.round(self.matrix.astype('float') / self.matrix.sum(axis=1)[:, np.newaxis], 3)
+        print('ROW-NORMALIZED:\n', row_normalized_mat)
+        col_normalized_mat = np.round(self.matrix.astype('float') / self.matrix.sum(axis=0)[np.newaxis, :], 3)
+        print('COLUMN-NORMALIZED:\n', col_normalized_mat)
 
-        fmt = '.2f' if normalize else 'd'
-        thresh = self.matrix.max() / 2.
-        for i, j in itertools.product(range(self.matrix.shape[0]), range(self.matrix.shape[1])):
-            plt.text(j, i, format(self.matrix[i, j], fmt), horizontalalignment="center",
-                     color="white" if self.matrix[i, j] > thresh else "black")
+        matrices_to_save = [self.matrix, row_normalized_mat, col_normalized_mat]
 
-        plt.tight_layout()
-        plt.ylabel('True label')
-        plt.xlabel('Predicted label')
-        plt.savefig('image_offload/conf_matrix.png')
-        plt.close()
+        for matrix_idx in range(3):
+            name = 'Counts'
+            if matrix_idx == 1:
+                name = 'Recall'
+            elif matrix_idx == 2:
+                name = 'Precision'
+
+            plt.imshow(matrices_to_save[matrix_idx], interpolation='nearest', cmap='Blues')
+            plt.title(name)
+            plt.colorbar()
+            tick_marks = np.arange(len(classes))
+            plt.xticks(tick_marks, classes)
+            plt.yticks(tick_marks, classes)
+
+            fmt = 'd' if matrix_idx == 0 else '.3f'
+            thresh = matrices_to_save[matrix_idx].max() / 2.
+            for i, j in itertools.product(range(matrices_to_save[matrix_idx].shape[0]),
+                                          range(matrices_to_save[matrix_idx].shape[1])):
+                plt.text(j, i, format(matrices_to_save[matrix_idx][i, j], fmt), horizontalalignment="center",
+                         color="white" if matrices_to_save[matrix_idx][i, j] > thresh else "black")
+
+            plt.tight_layout()
+            plt.ylabel('True label')
+            plt.xlabel('Predicted label')
+
+            plt.savefig('image_offload/conf_matrix_' + name + '.png')
+            plt.close()
+
+        print("matrices saved in /image_offload/ directory.")
