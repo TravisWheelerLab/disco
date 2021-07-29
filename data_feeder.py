7dbf2fae045ec58590a8ce695ccfe79227d7cac5
kaylairish <kayla.a.irish@gmail.com>
2021-07-29 11:07:26 -0600

    Add function to plot lengths in datasets

--- data_feeder.py
+++ data_feeder.py
@@ -8,6 +8,7 @@ import torch
 import pdb
 
 LABEL_TO_INDEX = {'A': 0, 'B': 1, 'X': 2}
+INDEX_TO_LABEL = {0: 'A', 1: 'B', 2: 'X'}
 
 
 class SpectrogramDataset(torch.utils.data.Dataset):
@@ -17,6 +18,7 @@ class SpectrogramDataset(torch.utils.data.Dataset):
                  max_spec_length=40,
                  filtered_sounds=['C', 'Y'],
                  clip_spects=True):
+        self.spect_lengths = defaultdict(list)
         self.directory_name = directory_name
         self.max_spec_length = max_spec_length
         self.filtered_sounds = filtered_sounds
@@ -39,21 +41,22 @@ class SpectrogramDataset(torch.utils.data.Dataset):
             if label not in filtered_labels and spect.shape[1] >= self.max_spec_length:
                 class_counter[label] += 1
                 spectrograms_list.append([label, spect])
+            self.spect_lengths[label].append(spect.shape[1])
         sorted_class_counter = OrderedDict(sorted(class_counter.items()))
         return spectrograms_list, sorted_class_counter
 
     def __getitem__(self, idx):
-        # returns a tuple with [0] the class label and [1] the spectrogram slice
-        # which is based on self.max_spec_length.
+        # returns a tuple with [0] the class label and [1] a slice of the spectrogram or the entire image.
         label = self.spectrograms_list[idx][0]
         spect = self.spectrograms_list[idx][1]
         num_col = spect.shape[1]
-        random_index = round(random.uniform(0, num_col - self.max_spec_length))
+        # random_index = round(random.uniform(0, num_col - self.max_spec_length))
+        random_index = 0
         if self.clip_spects:
-            spect_slice = torch.tensor(spect[:, random_index:random_index + self.max_spec_length]).unsqueeze(0)
+            spect_slice = torch.tensor(spect[:, random_index:random_index + self.max_spec_length])
             label_tensor = torch.tensor(np.repeat(a=LABEL_TO_INDEX[label], repeats=self.max_spec_length))
         else:
-            spect_slice = torch.tensor(spect).unsqueeze(0)
+            spect_slice = torch.tensor(spect)
             label_tensor = torch.tensor(np.repeat(a=LABEL_TO_INDEX[label], repeats=len(spect[1])))
         return spect_slice, label_tensor
 
@@ -61,19 +64,52 @@ class SpectrogramDataset(torch.utils.data.Dataset):
         return len(self.spectrograms_list)
 
     def generate_bar_chart(self):
-        # saves bar chart created from unique_labels dictionary into 'image_offload' directory
+        # saves bar chart created from unique_labels dictionary into 'image_offload' directory.
         labels = list(self.unique_labels.keys())
         counts = list(self.unique_labels.values())
-        plt.bar(labels, counts)
+        plt.style.use("dark_background")
+        plt.bar(labels, counts, color='hotpink')
+        plt.title('Bar chart of counts of each class in ' + self.directory_name)
         plt.savefig('image_offload/' + "bar_chart_" + self.directory_name + '.png')
+        plt.close()
+
+    def generate_lengths_histograms(self, plotted_sound_types=['A','B','X'], plot_all=True):
+        for sound_type in plotted_sound_types:
+            plt.style.use("dark_background")
+            plt.hist(self.spect_lengths[sound_type], bins=25, color='lightskyblue')
+            plt.title('lengths histogram of ' + sound_type + ' in ' + self.directory_name)
+            plt.show()
+            file_title = 'lengths_histogram_' + sound_type + '_' + self.directory_name + '.png'
+            plt.savefig('image_offload/' + file_title)
+            print('saved ' + file_title + '.')
+            plt.close()
+
+        if plot_all:
+            all_lengths = []
+            for sound_type, lengths_list in self.spect_lengths.items():
+                for length in lengths_list:
+                    all_lengths.append(length)
+            plt.style.use("dark_background")
+            plt.hist(all_lengths, bins=25, color='aquamarine')
+            plt.title('histogram, all lengths in ' + self.directory_name)
+            plt.show()
+            file_title = 'lengths_histogram_' + 'all_lengths_' + self.directory_name + '.png'
+            plt.savefig('image_offload/' + file_title)
+            print('saved ' + file_title)
+            plt.close()
 
     def get_unique_labels(self):
         return self.unique_labels.keys()
+
+    def generate_data_graphs(self):
+        self.generate_bar_chart()
+        self.generate_lengths_histograms()
 
 
 if __name__ == '__main__':
     train_data = SpectrogramDataset(directory_name="train_data", clip_spects=False)
     train_data.generate_bar_chart()
-    print(len(train_data))
-    print(train_data[25])
-    print('spect size:', train_data[25][0].shape)
+    train_data.generate_lengths_histograms()
+    test_data = SpectrogramDataset(directory_name="test_data", clip_spects=False)
+    test_data.generate_bar_chart()
+    test_data.generate_lengths_histograms()
