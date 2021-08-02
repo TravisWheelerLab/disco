ae803588721309c79f78ec07f483498bd6377094
kaylairish <kayla.a.irish@gmail.com>
2021-08-02 17:47:42 -0600

    Update to incorporate naming scheme

--- move_files_to_valid.py
+++ move_files_to_valid.py
@@ -1,11 +1,19 @@
 from glob import glob
 import os
-
+import spectrogram_analysis as sa
 from random import shuffle
 
 # this script moves half of the test data files to a validation directory.
 
-spect_type_to_move_into_valid = 'mel_log_1600_vert_trim_15'
+mel = True
+log = True
+n_fft = 1600
+vert_trim = None
+
+if vert_trim is None:
+    vert_trim = sa.determine_default_vert_trim(mel, log, n_fft)
+
+spect_type_to_move_into_valid = sa.form_spectrogram_type(mel, n_fft, log, vert_trim)
 
 files = glob(os.path.join('data/test/' + spect_type_to_move_into_valid + '/spect', "*npy"))
 shuffle(files)
