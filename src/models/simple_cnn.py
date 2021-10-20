d2139c617ae70f3f8f0531176ba11c519d344b38
thomas colligan <thomas.colligan@umontana.edu>
2021-10-20 09:59:22 -0600

    Add masked forward to the model and associated new hyperparameters in the training script

--- src/models/simple_cnn.py
+++ src/models/simple_cnn.py
@@ -4,6 +4,8 @@ import torch
 import torchmetrics
 import numpy as np
 
+MASK_CHARACTER = -1
+
 __all__ = ['SimpleCNN']
 
 
@@ -16,7 +18,12 @@ class SimpleCNN(pl.LightningModule):
                  apply_log,
                  n_fft,
                  begin_cutoff_idx,
-                 vertical_trim):
+                 vertical_trim,
+                 mask_beginning_and_end,
+                 begin_mask,
+                 end_mask,
+                 train_files,
+                 val_files):
 
         super(SimpleCNN, self).__init__()
         self.conv1 = torch.nn.Conv1d(in_channels=in_channels, out_channels=256, kernel_size=3, padding=1)
@@ -37,10 +44,37 @@ class SimpleCNN(pl.LightningModule):
         self.begin_cutoff_idx = begin_cutoff_idx
         self.vertical_trim = vertical_trim
         self.in_channels = in_channels
+        self.mask_beginning_and_end = mask_beginning_and_end
+        self.begin_mask = begin_mask
+        self.end_mask = end_mask
+        self.train_files = train_files
+        self.val_files = val_files
 
         self.save_hyperparameters()
 
-    def forward(self, x):
+    def _masked_forward(self, x, x_mask):
+        x = self.conv1(x)
+        x = torch.nn.functional.relu(x)
+        x = self.conv2(x)
+        x = torch.nn.functional.relu(x)
+        x = self.conv3(x)
+        x = torch.nn.functional.relu(x)
+        x = self.conv4(x)
+        x = torch.nn.functional.relu(x)
+        x = self.conv5(x)
+        x = torch.nn.functional.relu(x)
+        x = self.conv6(x)
+        x = torch.nn.functional.relu(x)
+        x = self.conv7(x)
+        x = torch.nn.functional.relu(x)
+        x = self.conv8(x)
+        x = torch.nn.functional.relu(x)
+        x = self.conv9(x)
+        x[x_mask.expand(-1, 3, -1)] = 0
+        output = torch.nn.functional.log_softmax(x, dim=1)
+        return output
+
+    def _forward(self, x):
         x = self.conv1(x)
         x = torch.nn.functional.relu(x)
         x = self.conv2(x)
@@ -61,12 +95,31 @@ class SimpleCNN(pl.LightningModule):
         output = torch.nn.functional.log_softmax(x, dim=1)
         return output
 
+    def forward(self, x, mask=None):
+        if mask is not None:
+            logits = self._masked_forward(x, mask)
+        else:
+            logits = self._forward(x)
+
+        return logits
+
     def _shared_step(self, batch):
-        x, y = batch
-        logits = self.forward(x)
-        loss = F.nll_loss(logits, y)
-        preds = logits.argmax(dim=1)
-        acc = self.accuracy(preds, y)
+        if len(batch) == 3:
+            # oof masked batch
+            x, x_mask, y = batch
+            logits = self._masked_forward(x, x_mask)
+            loss = F.nll_loss(logits, y, ignore_index=MASK_CHARACTER)
+            preds = logits.argmax(dim=1)
+            preds = preds[y != MASK_CHARACTER]
+            labels = y[y != MASK_CHARACTER]
+            acc = self.accuracy(preds, labels)
+        else:
+            x, y = batch
+            logits = self.forward(x)
+            loss = F.nll_loss(logits, y)
+            preds = logits.argmax(dim=1)
+            acc = self.accuracy(preds, y)
+
         return loss, acc
 
     def training_step(self, batch, batch_nb):
@@ -88,6 +141,9 @@ class SimpleCNN(pl.LightningModule):
         self.log('train_loss', loss)
         self.log('train_acc', acc)
         self.log('learning_rate', self.learning_rate)
+
+    def on_train_start(self):
+        self.log('hp_metric', self.learning_rate + self.n_fft)
 
     def validation_epoch_end(self, outputs):
         val_loss = self.all_gather([x['val_loss'] for x in outputs])
--- src/train.py
+++ src/train.py
@@ -10,10 +10,11 @@ import torch
 import pytorch_lightning as pl
 import spectrogram_analysis as sa
 from pytorch_lightning.plugins import DDPPlugin
+from glob import glob
 
 from models import SimpleCNN
 from argparse import ArgumentParser
-from dataset import SpectrogramDataset
+from dataset import SpectrogramDatasetMultiLabel, pad_batch
 
 DEFAULT_SPECTROGRAM_NUM_ROWS = 128
 
@@ -33,7 +34,12 @@ def train_parser():
     tunable.add_argument('--vertical_trim', type=int, required=True,
                          help='how many rows to remove from the low-frequency range of the spectrogram.'
                               'This is probably unnecessary because NaNs are easily removed in preprocessing.')
-
+    tunable.add_argument('--mask_beginning_and_end', action='store_true',
+                         help='whether or not to mask the beginning and end of single-label chirps')
+    tunable.add_argument('--begin_mask', type=int, default=30,
+                         help='how many cols to mask from beginning')
+    tunable.add_argument('--end_mask', type=int, default=10,
+                         help='how many cols to mask from end')
     non_tunable = ap.add_argument_group(title='non-tunable args', description='the "mel" argument depends on the data'
                                                                               'extraction step - whether or not a mel'
                                                                               'spectrogram was computed')
@@ -72,52 +78,59 @@ def train_parser():
 
 def train_func(hparams):
     # TODO: Don't hardcode this.
+
     spect_type = 'mel_no_log_{}_no_vert_trim'.format(hparams.n_fft)
+    print(spect_type)
 
-    train_dataset = SpectrogramDataset('train',
-                                       spect_type=spect_type,
-                                       data_path=hparams.data_path,
-                                       apply_log=hparams.log,
-                                       vertical_trim=hparams.vertical_trim,
-                                       begin_cutoff_idx=hparams.begin_cutoff_idx,
-                                       bootstrap_sample=hparams.bootstrap,
-                                       clip_spects=True)
+    train_files = glob(os.path.join(hparams.data_path, 'train', spect_type, "*"))
+    val_files = glob(os.path.join(hparams.data_path, 'validation', spect_type, "*"))
 
-    test_dataset = SpectrogramDataset(dataset_type='test',
-                                      data_path=hparams.data_path,
-                                      spect_type=spect_type,
-                                      apply_log=hparams.log,
-                                      vertical_trim=hparams.vertical_trim,
-                                      begin_cutoff_idx=hparams.begin_cutoff_idx,
-                                      clip_spects=False,
-                                      bootstrap_sample=False)
+    train_dataset = SpectrogramDatasetMultiLabel(train_files,
+                                                 apply_log=hparams.log,
+                                                 vertical_trim=hparams.vertical_trim,
+                                                 bootstrap_sample=hparams.bootstrap,
+                                                 mask_beginning_and_end=hparams.mask_beginning_and_end,
+                                                 begin_mask=hparams.begin_mask,
+                                                 end_mask=hparams.end_mask)
 
-    train_loader = torch.utils.data.DataLoader(train_dataset,
-                                               batch_size=hparams.batch_size,
-                                               shuffle=True,
-                                               drop_last=False,
-                                               num_workers=hparams.num_workers)
+    val_dataset = SpectrogramDatasetMultiLabel(val_files,
+                                               apply_log=hparams.log,
+                                               vertical_trim=hparams.vertical_trim,
+                                               bootstrap_sample=hparams.bootstrap,
+                                               mask_beginning_and_end=hparams.mask_beginning_and_end,
+                                               begin_mask=hparams.begin_mask,
+                                               end_mask=hparams.end_mask)
     # batch size of 1 since we're evaluating on lots of differently sized
     # labeled regions. Could implement a mask to zero out bits of predictions
     # but this is too much work for the amount of time it takes to evaluate our validation
     # set.
-    test_loader = torch.utils.data.DataLoader(test_dataset,
-                                              batch_size=1,
-                                              shuffle=False,
-                                              num_workers=hparams.num_workers)
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=hparams.batch_size,
+                                               shuffle=True,
+                                               num_workers=hparams.num_workers,
+                                               collate_fn=None if hparams.batch_size == 1 else pad_batch)
 
-    # i need to finetune on large spectrograms with multiple different classes so
-    # context is learned
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=hparams.batch_size,
+                                             shuffle=False,
+                                             num_workers=hparams.num_workers,
+                                             collate_fn=None if hparams.batch_size == 1 else pad_batch)
 
     in_channels = DEFAULT_SPECTROGRAM_NUM_ROWS - hparams.vertical_trim
 
+    # TODO: refactor this to incorporate data loaders more easily.
     model = SimpleCNN(in_channels,
                       hparams.learning_rate,
                       hparams.mel,
                       hparams.log,
                       hparams.n_fft,
                       hparams.begin_cutoff_idx,
-                      hparams.vertical_trim)
+                      hparams.vertical_trim,
+                      hparams.mask_beginning_and_end,
+                      hparams.begin_mask,
+                      hparams.end_mask,
+                      train_dataset.files,
+                      val_dataset.files)
 
     save_best = pl.callbacks.model_checkpoint.ModelCheckpoint(
         monitor='val_loss',
@@ -146,14 +159,14 @@ def train_func(hparams):
             **trainer_kwargs
         )
 
-        trainer.tune(model, train_loader, test_loader)
+        trainer.tune(model, train_loader, val_loader)
 
     else:
         trainer = pl.Trainer(
             **trainer_kwargs
         )
 
-    trainer.fit(model, train_loader, test_loader)
+    trainer.fit(model, train_loader, val_loader)
 
     torch.save(model.state_dict(), os.path.join(trainer.log_dir, hparams.model_name))
 
