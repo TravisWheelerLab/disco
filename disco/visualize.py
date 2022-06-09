157ecd02ce983e66b0066e9c66050f3c146d3d4d
kaylairish <kayla.a.irish@gmail.com>
2022-06-09 15:55:09 -0600

    Mass overhaul code structure

--- disco/visualize.py
+++ disco/visualize.py
@@ -4,7 +4,13 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 import disco.inference_utils as infer
 
-import disco.heuristics as heuristics
+
+class Visualizer:
+    def __init__(self, data_path, config):
+        self.config = config
+        self.spectrogram, self.medians, self.post_hmm, self.iqr, self.means, self.votes = load_arrays(data_path)
+        self.spectrogram = np.flip(self.spectrogram, axis=0)
+        self.median_argmax = np.argmax(self.medians, axis=0)
 
 
 def load_arrays(data_root):
@@ -12,10 +18,12 @@ def load_arrays(data_root):
     spectrogram = infer.load_pickle(os.path.join(data_root, "raw_spectrogram.pkl"))
     post_hmm = infer.load_pickle(os.path.join(data_root, "hmm_predictions.pkl"))
     iqr = infer.load_pickle(os.path.join(data_root, "iqrs.pkl"))
-    return medians, spectrogram, post_hmm, iqr
+    means = infer.load_pickle(os.path.join(data_root, "mean_predictions.pkl"))
+    votes = infer.load_pickle(os.path.join(data_root, "votes.pkl"))
+    return spectrogram, medians, post_hmm, iqr, means, votes
 
 
-def visualize(config, data_path, hop_length, sample_rate):
+def visualize(config, data_path):
     """
     Visualize predictions interactively.
     :param config:
@@ -24,35 +32,26 @@ def visualize(config, data_path, hop_length, sample_rate):
     :param sample_rate:
     :return:
     """
-    # TODO: refactor so this function isn't so massive
     fig, ax = plt.subplots(sharex=True, nrows=2, figsize=(10, 7))
 
-    medians, spectrogram, post_hmm, iqr = load_arrays(data_path)
-    iqr_no_mods = iqr.copy()
-    median_argmax = np.argmax(medians, axis=0)
+    visualizer = Visualizer(data_path, config)
 
     for class_index, name in config.class_code_to_name.items():
-        all_class = median_argmax == class_index
+        all_class = visualizer.median_argmax == class_index
         x = range(0, all_class.shape[-1])
         ax[1].fill_between(x, 15, 19, where=all_class, color=config.name_to_rgb_code[name])
 
-    post_hmm = infer.smooth_predictions_with_hmm(median_argmax, config=config)
-    post_hmm = heuristics.remove_a_chirps_in_between_b_chirps(
-        post_hmm, iqr_no_mods, config.name_to_class_code
-    )
-
     for class_index, name in config.class_code_to_name.items():
-        all_class = post_hmm == class_index
+        all_class = visualizer.post_hmm == class_index
         x = range(0, all_class.shape[-1])
         ax[1].fill_between(
             x, 10, 14, where=all_class, color=config.name_to_rgb_code[name]
         )
 
     ax[1].axis("off")
-    spectrogram = np.flip(spectrogram, axis=0)
 
-    ax[0].imshow(spectrogram, aspect="auto", origin="lower")
-    ax[0].set_ylim([0, spectrogram.shape[0]])
+    ax[0].imshow(visualizer.spectrogram, aspect="auto", origin="lower")
+    ax[0].set_ylim([0, visualizer.spectrogram.shape[0]])
 
     ax[0].set_title("Raw spectrogram")
     ax[0].set_ylabel("frequency bin")
@@ -62,18 +61,18 @@ def visualize(config, data_path, hop_length, sample_rate):
     plt.subplots_adjust()
     n = 1200
     ax[1].axis([0, n, 4, 20])
-    ax[0].axis([0, n, 0, spectrogram.shape[0]])
+    ax[0].axis([0, n, 0, visualizer.spectrogram.shape[0]])
     p = ax[0].get_position()
     ax[1].set_position([p.x0, p.y0 - 0.1, p.x1 - p.x0, 0.09])
     fig.text(p.x0 - 0.08, p.y0 - 0.03, "ensemble prediction", fontsize=8)
     fig.text(p.x0 - 0.08, p.y0 - 0.06, "post processed", fontsize=8)
 
     axpos = plt.axes([p.x0, p.y0 - 0.2, p.x1 - p.x0, 0.05])
-    spos = Slider(axpos, "x-position", 0.0, medians.shape[1])
+    spos = Slider(axpos, "x-position", 0.0, visualizer.medians.shape[1])
 
     def update(val):
         ax[1].axis([spos.val, spos.val + n, 4, 20])
-        ax[0].axis([spos.val, spos.val + n, 0, spectrogram.shape[0]])
+        ax[0].axis([spos.val, spos.val + n, 0, visualizer.spectrogram.shape[0]])
 
     spos.on_changed(update)
     plt.show()
