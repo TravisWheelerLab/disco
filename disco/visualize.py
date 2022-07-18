5076b887bcc30c95bef712caa8649aadfc9aae06
kaylairish <kayla.a.irish@gmail.com>
2022-07-18 12:18:57 -0600

    Move script to functions for increased readability

--- disco/visualize.py
+++ disco/visualize.py
@@ -35,6 +35,14 @@ class Visualizer:
                 text = "votes for " + config.class_code_to_name[class_code]
                 self.statistics.append((text, self.votes[class_code, :]))
 
+        # Build the figure height based on the height of the spectrogram and the amount of statistics to display.
+        self.num_statistics_plus_slider = len(self.statistics) + 1
+        self.spect_ht = 2.5
+        statistics_ht = (0.5+(self.num_statistics_plus_slider-1) + (self.num_statistics_plus_slider-2)*0.1)*0.22
+        self.fig_ht = self.spect_ht + statistics_ht
+
+        self.height_ratios = get_subplot_ht_ratios(statistics_ht, self.num_statistics_plus_slider, self.spect_ht)
+
 
 def load_arrays(data_root):
     medians = infer.load_pickle(os.path.join(data_root, "median_predictions.pkl"))
@@ -46,51 +54,21 @@ def load_arrays(data_root):
     return spectrogram, medians, post_hmm, iqr, means, votes
 
 
-def add_predictions_legend(ax, config):
-    legend_handles = []
-    for name in config.name_to_rgb_code.keys():
-        icon = mlines.Line2D([], [], color=config.name_to_rgb_code[name], marker="s", linestyle='None', markersize=10,
-                             label=name.title())
-        legend_handles.append(icon)
-    ax.legend(handles=legend_handles, loc='upper right', fontsize='small', title='prediction')
-
-
-def visualize(config, data_path, medians, post_process, means, iqr, votes):
-    """
-    Visualize predictions interactively.
-    :param config: disco.Config() object.
-    :param data_path: path of directory containing spectrogram and inference ran on it.
-    :param medians: whether to display median predictions by the ensemble.
-    :param post_process: whether to display post-processed (hmm, other heuristics) predictions by the ensemble.
-    :param means: whether to display mean predictions by the ensemble.
-    :return:
-    """
-    visualizer = Visualizer(data_path, medians, post_process, means, iqr, votes, config)
-
-    # Build the figure height based on the height of the spectrogram and the amount of statistics we want to display.
-    num_statistics_displayed = len(visualizer.statistics)
-    num_statistics_plus_slider = num_statistics_displayed + 1
-    spect_ht = 2.5
-    statistics_ht = (0.5 + (num_statistics_plus_slider - 1) + (num_statistics_plus_slider - 2)*0.1) * 0.22
-    fig_ht = spect_ht + statistics_ht
-
+def get_subplot_ht_ratios(height_of_statistics_portion, num_statistics_plus_slider, spectrogram_height):
     # Create width ratios so the spectrogram's window is bigger than the statistics below it, and the
     #   statistics all have the same size.
-    statistics_display_sizes = np.repeat(statistics_ht/num_statistics_plus_slider, num_statistics_plus_slider).tolist()
-    subplot_sizes = [spect_ht] + statistics_display_sizes
+    statistics_display_sizes = np.repeat(height_of_statistics_portion/num_statistics_plus_slider,
+                                         num_statistics_plus_slider).tolist()
+    subplot_sizes = [spectrogram_height] + statistics_display_sizes
     height_ratios = {'height_ratios': subplot_sizes}
+    return height_ratios
 
-    fig, axs = plt.subplots(sharex=True, nrows=num_statistics_plus_slider+1,
-                            figsize=(10, fig_ht), gridspec_kw=height_ratios)
-    fig.subplots_adjust(top=1 - 0.35 / fig_ht, bottom=0.15 / fig_ht, left=0.2, right=0.99)
 
-    # Show spectrogram
-    axs[0].imshow(visualizer.spectrogram, aspect="auto")
-
+def imshow_statistics_rows(axs, visualizer, config):
     # Show each statistics row
-    for i in range(1, len(axs)-1):
-        label = visualizer.statistics[i-1][0]
-        statistics_bar = np.expand_dims(visualizer.statistics[i-1][1], axis=0)
+    for i in range(1, len(axs) - 1):
+        label = visualizer.statistics[i - 1][0]
+        statistics_bar = np.expand_dims(visualizer.statistics[i - 1][1], axis=0)
         if "preds" in label or "post process" in label:
             color_dict = dict()
             for class_code in range(len(config.class_code_to_name.keys())):
@@ -111,15 +89,49 @@ def visualize(config, data_path, medians, post_process, means, iqr, votes):
     for i in range(1, len(axs)):
         axs[i].set_axis_off()
 
+
+def add_predictions_legend(ax, config):
+    legend_handles = []
+    for name in config.name_to_rgb_code.keys():
+        icon = mlines.Line2D([], [], color=config.name_to_rgb_code[name], marker="s", linestyle='None', markersize=10,
+                             label=name.title())
+        legend_handles.append(icon)
+    ax.legend(handles=legend_handles, loc='upper right', fontsize='small', title='prediction')
+
+
+def build_slider(axs, visualizer):
+    spect_position = axs[len(visualizer.statistics) + 1].get_position()
+    axis_position = plt.axes([spect_position.x0, spect_position.y0, spect_position.x1 - spect_position.x0, 0.05])
+    slider = Slider(axis_position, "x-position", 0.0, visualizer.medians.shape[1])  # todo: fix this to not be medians
+    return slider
+
+
+def visualize(config, data_path, medians, post_process, means, iqr, votes):
+    """
+    Visualize predictions interactively.
+    :param config: disco.Config() object.
+    :param data_path: path of directory containing spectrogram and inference ran on it.
+    :param medians: whether to display median predictions by the ensemble.
+    :param post_process: whether to display post-processed (hmm, other heuristics) predictions by the ensemble.
+    :param means: whether to display mean predictions by the ensemble.
+    :return:
+    """
+    visualizer = Visualizer(data_path, medians, post_process, means, iqr, votes, config)
+
+    fig, axs = plt.subplots(sharex=True, nrows=visualizer.num_statistics_plus_slider + 1,
+                            figsize=(10, visualizer.fig_ht), gridspec_kw=visualizer.height_ratios)
+    fig.subplots_adjust(top=1-0.35/visualizer.fig_ht, bottom=0.15/visualizer.fig_ht, left=0.2, right=0.99)
+
+    axs[0].imshow(visualizer.spectrogram, aspect="auto")
+    imshow_statistics_rows(axs, visualizer, config)
+
     if visualizer.show_legend:
         add_predictions_legend(axs[0], config)
 
-    spect_position = axs[len(visualizer.statistics)+1].get_position()
-    axis_position = plt.axes([spect_position.x0, spect_position.y0, spect_position.x1 - spect_position.x0, 0.05])
-    slider = Slider(axis_position, "x-position", 0.0, visualizer.medians.shape[1]) # todo: fix this to not be medians
+    slider = build_slider(axs, visualizer)
 
     def update(val):
-        for i in range(len(axs)-1):
+        for i in range(len(axs) - 1):
             axs[i].set_xlim(slider.val, slider.val + config.visualization_zoom_out)
 
     slider.on_changed(update)
