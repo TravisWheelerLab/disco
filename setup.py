8ae1574762bb74e669077710bdbb485f6f204675
thomas colligan <thomas.colligan@umontana.edu>
2022-10-27 13:23:07 -0600

    Add disco to requirements and a setup.py file for installing via git+pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="disco",
+    version="0.0.1",
+    author="Wheeler Lab",
+    author_email="colligan@arizona.edu",
+    description="Analyze spectrograms with deep learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TravisWheelerLab/disco",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires=">=3.8",
+)
