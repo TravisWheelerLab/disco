Fix bug in eventwise metric computation, start repro'ing plots in paper (again...)

disco/accuracy_metrics.py
@@ -26,7 +26,7 @@ class SoundEvent:
         ground_truth_array,
         predictions_array,
         avg_iqr_array,
-        votes_array,
+        proportion_event_correct,
     ):
         self.start = start
         self.end = end
@@ -46,13 +46,10 @@ class SoundEvent:
         else:
             self.correct = False
             if self.predictions_mode == self.ground_truth_label:
-                self.predictions_mode = 3
+                self.predictions_mode = 2
 
         self.span_iqr_average = np.average(avg_iqr_array)
-        self.span_votes_average = np.average(votes_array)
-        if (self.span_iqr_average > iqr_threshold) or (
-            self.span_votes_average < min_votes_needed
-        ):
+        if self.span_iqr_average > iqr_threshold:
             self.adjusted_preds_label = name_to_class_code["BACKGROUND"]
         else:
             self.adjusted_preds_label = self.predictions_mode
@@ -64,11 +61,11 @@ def load_accuracy_metric_pickles(data_path, return_dict=True):
     post_hmm = infer.load_pickle(os.path.join(data_path, "hmm_predictions.pkl"))
     iqr = infer.load_pickle(os.path.join(data_path, "iqrs.pkl"))
     votes = infer.load_pickle(os.path.join(data_path, "votes.pkl"))
-    average_iqr = np.mean(iqr, axis=0)
-    median_argmax = np.argmax(medians, axis=0)
+    spect = infer.load_pickle(os.path.join(data_path, "spectrogram.pkl"))
     if return_dict:
         return {
             "ground_truth": ground_truth,
+            "spectrogram": spect,
             "medians": medians,
             "post_hmm": post_hmm,
             "iqr": iqr,
@@ -144,9 +141,7 @@ def adjust_preds_by_confidence(
     return median_argmax
 
 
-def make_sound_events_array(
-    ground_truth, median_argmax, average_iqr, winning_vote_count
-):
+def make_sound_events_array(ground_truth, median_argmax, average_iqr):
     sound_event_indices = get_sound_event_indices(ground_truth)
 
     sound_events = []
@@ -156,9 +151,7 @@ def make_sound_events_array(
             end = sound_event_indices[i + 1]
         else:
             end = len(ground_truth)
-        sound_event = SoundEvent(
-            start, end, ground_truth, median_argmax, average_iqr, winning_vote_count
-        )
+        sound_event = SoundEvent(start, end, ground_truth, median_argmax, average_iqr)
         sound_events.append(sound_event)
 
     return sound_events
@@ -202,8 +195,40 @@ def get_accuracy_metrics(ground_truth, predictions, normalize_confmat=None):
     return accuracy, recall, precision, confusion_matrix, confusion_matrix_nonnorm, IoU
 
 
-def eventwise_metrics(data_dict):
-    pass
+def eventwise_metrics(data_dict, cov_pct=0.5):
+    gts = data_dict["ground_truth"]
+    spect = data_dict["spectrogram"]
+    preds = data_dict["medians"]
+    for cov_pct in np.linspace(0.1, 1.0, num=10):
+        y_true = []
+        y_pred = []
+        for ground_truth, spect_slice, pred in zip(gts, spect, preds):
+            pred = np.argmax(pred, axis=0)
+            if len(np.unique(ground_truth)) == 1:
+                if np.sum(ground_truth == pred) >= cov_pct * ground_truth.shape[0]:
+                    y_pred.append(ground_truth[0])
+                else:
+                    y_pred.append(cfg.name_to_class_code["BACKGROUND"])
+
+                y_true.append(ground_truth[0])
+            else:
+                slice_boundaries = np.where(np.diff(ground_truth) != 0)[0]
+                end_idx = (
+                    [0] + [x + 1 for x in slice_boundaries] + [ground_truth.shape[0]]
+                )
+                for i in range(len(end_idx) - 1):
+
+                    gt_slice = ground_truth[end_idx[i] : end_idx[i + 1]]
+                    y_true.append(gt_slice[0])
+                    assert len(np.unique(gt_slice) == 1)
+                    pred_slice = pred[end_idx[i] : end_idx[i + 1]]
+
+                    if np.sum(gt_slice == pred_slice) >= cov_pct * gt_slice.shape[0]:
+                        y_pred.append(ground_truth[0])
+                    else:
+                        y_pred.append(cfg.name_to_class_code["BACKGROUNd"])
+
+        print(cov_pct, metrics.recall_score(y_true, y_pred, average=None))
 
 
 def pointwise_metrics(data_dict):
@@ -263,4 +288,4 @@ if __name__ == "__main__":
     args = ap.parse_args()
 
     data = load_accuracy_metric_pickles(args.infer_data_root)
-    x = pointwise_metrics(data)
+    x = eventwise_metrics(data)

disco/evaluate_beetles_test_files.py
@@ -13,11 +13,12 @@ from disco.util.loading import load_model_class
 experiment = Experiment()
 
 
+@torch.no_grad()
 @experiment.automain
 def evaluate_test_files(
     test_path,
-    model_name,
     metrics_path,
+    model_name="UNet1D",
     saved_model_directory=None,
     tile_size=1024,
     num_threads=4,
@@ -64,14 +65,17 @@ def evaluate_test_files(
         test_loader, models, device=device
     )
 
-    predictions = np.argmax(medians, axis=0).squeeze()
+    predictions = [np.argmax(m, axis=0).squeeze() for m in medians]
 
-    hmm_predictions = infer.smooth_predictions_with_hmm(
-        predictions,
-        cfg.hmm_transition_probabilities,
-        cfg.hmm_emission_probabilities,
-        cfg.hmm_start_probabilities,
-    )
+    hmm_predictions = [
+        infer.smooth_predictions_with_hmm(
+            p,
+            cfg.hmm_transition_probabilities,
+            cfg.hmm_emission_probabilities,
+            cfg.hmm_start_probabilities,
+        )
+        for p in predictions
+    ]
 
     os.makedirs(metrics_path, exist_ok=True)
 
@@ -83,10 +87,10 @@ def evaluate_test_files(
     votes_path = os.path.join(metrics_path, "votes.pkl")
     spectrogram_path = os.path.join(metrics_path, "spectrogram.pkl")
 
-    infer.pickle_tensor(labels, labels_path)
-    infer.pickle_tensor(hmm_predictions, hmm_prediction_path)
-    infer.pickle_tensor(means, mean_path)
-    infer.pickle_tensor(medians, median_prediction_path)
-    infer.pickle_tensor(iqr, iqr_path)
-    infer.pickle_tensor(votes, votes_path)
-    infer.pickle_tensor(spect, spectrogram_path)
+    infer.pickle_object(labels, labels_path)
+    infer.pickle_object(hmm_predictions, hmm_prediction_path)
+    infer.pickle_object(means, mean_path)
+    infer.pickle_object(medians, median_prediction_path)
+    infer.pickle_object(iqr, iqr_path)
+    infer.pickle_object(votes, votes_path)
+    infer.pickle_object(spect, spectrogram_path)

disco/util/inference_utils.py
@@ -115,6 +115,11 @@ def convert_spectrogram_index_to_seconds(spect_idx, hop_length, sample_rate):
     return spect_idx * seconds_per_hop
 
 
+def pickle_object(data, path):
+    with open(path, "wb") as dst:
+        pickle.dump(data, dst)
+
+
 def pickle_tensor(data, path):
     """
     Pickles a pytorch tensor. If .numpy() isn't called, the entire computational graph is saved resulting in 100s of
@@ -491,23 +496,14 @@ def evaluate_test_loader(spectrogram_dataset, models, device="cpu"):
     for features, labels in spectrogram_dataset:
         features = features.to(device)
         ensemble_preds = predict_with_ensemble(models, features)
-
         ensemble_preds = np.stack(ensemble_preds)
         iqrs, medians, means, votes = calculate_ensemble_statistics(ensemble_preds)
-
-        spectrograms_concat.extend(features.to("cpu"))
-        labels_concat.extend(labels.to("cpu"))
+        spectrograms_concat.extend(features.to("cpu").numpy())
+        labels_concat.extend(labels.to("cpu").numpy())
         iqrs_full_sequence.extend(iqrs)
         medians_full_sequence.extend(medians)
         means_full_sequence.extend(means)
         votes_full_sequence.extend(votes)
-
-    spectrograms_concat = np.concatenate(spectrograms_concat, axis=-1)
-    labels_concat = np.concatenate(labels_concat, axis=-1)
-    iqrs_full_sequence = np.concatenate(iqrs_full_sequence, axis=-1)
-    medians_full_sequence = np.concatenate(medians_full_sequence, axis=-1)
-    means_full_sequence = np.concatenate(means_full_sequence, axis=-1)
-    votes_full_sequence = np.concatenate(votes_full_sequence, axis=-1)
 
     return (
         spectrograms_concat,